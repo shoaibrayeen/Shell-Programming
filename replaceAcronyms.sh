90a8cdd53ad68a26f8357e366b21256fb0628289
MOHD SHOAIB RAYEEN <31645414+shoaibrayeen@users.noreply.github.com>
2017-11-04 23:18:59 +0530

    Replace Acronyms from their full forms
    There must be two files for this program.
    User Entered File contains full name.
    acronyms file contains the acronyms

---
+++ replaceAcronyms.sh
@@ -0,0 +1,39 @@
+clear
+echo "This script replaces the acronyms by their full-forms"
+if [ $# -eq 0 ]
+then
+	echo "Enter a fileName"
+	read fname
+	while [ -z "$fname" ]
+	do
+		read fname
+	done
+else
+	fname=$1
+fi
+if [ ! -f "$fname" ]
+then
+	echo "File not exist."
+	exit 1
+fi
+if [ ! -r "$fname" ]
+then
+	echo "File not readable."
+	exit 2
+fi
+
+awk  'FILENAME~/acronyms/ { for(i=2;i<=NF;i++)
+				ar[$1]=ar[$1]$i" "
+			next }	
+	{
+		for(i=1;i<=NF;i++)
+		{
+			if($i in ar)
+			{
+				 k=$i
+				 $i=ar[$i] 
+				 delete ar[k]
+			}
+		}
+		print $0
+	}' "acronyms" "$fname"
