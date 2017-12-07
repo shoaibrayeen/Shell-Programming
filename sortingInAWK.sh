bef6b0a8b80e4e6257b2a94c23cadf3b8be966de
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-07 21:17:50 +0530

    Sorting an array

--- sortingInAWK.sh
+++ sortingInAWK.sh
@@ -1,28 +1,29 @@
 echo "This program Sort an array"
 awk 'BEGIN{	
 		print "Enter Size"
-		getline number
-    
+		getline n <"/dev/tty"
 		print "\nEnter array\n"
-		for( i=1 ; i <= number ; i++ ) {
-		    getline array[i]
-    }
-    
-	  for(j=2 ; j <= number ; j++) {
-			  key=array[j];
-			  i=j-1;
-        
-			  while(array[i]>key && i>0) {
-				    array[i+1]=array[i];
-				    i=i-1;
-			  }
-        
-			  array[i+1]=key
+		for( i=1 ; i<=n ; i++ )
+		getline a[i] <"/dev/tty"
+		insertion(a,n)
+	  }
+		function insertion ( a,n )
+		{
+	        	for(j=2;j<=n;j++)
+			{
+				 key=a[j];
+				 i=j-1;
+				while(a[i]>key && i>0)
+				{
+					a[i+1]=a[i];
+					i=i-1;
+				}
+				a[i+1]=key
+			}
 		}
+	END{
 		print "\nDisplay Array\n"
-		for( i=1 ; i<=number ; i++ ) {
-		    printf " " array[i]
-    }
-}'
+		for( i=1 ; i<=n ; i++ )
+		printf " " a[i]
+	}' /dev/null
 echo
-	
